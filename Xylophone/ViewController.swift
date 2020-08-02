5c4d49974dc07e47fe4c4e1b2ffe61f1f50ceeb2
saiwaimaung <saiwaimaung@gmail.com>
2020-08-02 20:31:36 +0800

    ADD: extended finishes up

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,28 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
+
+    func playSound(play key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.superview?.alpha = 0.5
+        let key = sender.currentTitle!
+        playSound(play: key)
+        sender.superview?.alpha = 1
+    }
 }
 
